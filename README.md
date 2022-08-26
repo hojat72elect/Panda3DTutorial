4d167abb6b70a97988cf0a69853f91651adead92
hojat <hojat72elect@gmail.com>
2022-08-25 23:39:04 -0400

    ShowBase object used for showing a window

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is the code I wrote by following <a href="https://youtu.be/LNMz52Pkl_U">these tutorial series</a> on YouTube.
 
-I'm currently at episode 1 at 00:00 .
+I'm currently at episode 4 at 00:01 .
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from direct.showbase.ShowBase import ShowBase
+
+
+# extends the [ShowBase] class
+class MyGame(ShowBase):
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    game = MyGame()
+    game.run()
